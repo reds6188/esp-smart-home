d2132abf28a0cbb2307d900a0091aef8172ad009
Daniele Rossi <73635936+reds6188@users.noreply.github.com>
2025-05-19 16:57:36 +0200

    Added "console" library

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
-#include <Arduino.h>
+#include "main.h"
 
 void setup() {
+	console.header(DOUBLE_DASHED, LOG_BLUE, 80, "START INITIALIZATION");
 	// put your setup code here, to run once:
+	console.header(DOUBLE_DASHED, LOG_BLUE, 80, "END INITIALIZATION");
 }
 
 void loop() {
--- src/main.h
+++ src/main.h
@@ -2,5 +2,6 @@
 #define MAIN_H_
 
 #include <Arduino.h>
+#include <console.h>
 
 #endif  /* MAIN_H_ */
