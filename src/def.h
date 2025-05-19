7f5c264ef6f4daad0e2be6977b206251d5f7d070
Daniele Rossi <73635936+reds6188@users.noreply.github.com>
2025-05-19 16:59:26 +0200

    Added header file "def"

---
+++ src/def.h
@@ -0,0 +1,15 @@
+#ifndef DEF_H_
+#define DEF_H_
+
+// ================================================================================================
+// Pin definitions for ESP32 Proto Board V1 (created by Daniele Rossi) ============================
+// ================================================================================================
+
+#define BTN_1		34
+#define BTN_2		35
+
+#define RED_LED		26
+#define YLW_LED		27
+#define GRN_LED		14
+
+#endif  /* DEF_H_ */
